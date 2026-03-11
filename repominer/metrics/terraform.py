68f0b27e09ad746d057e1f8a2d6557f72da308e1
franc <f.fuccio3@studenti.unisannio.it>
2026-03-11 15:33:37 +0100

    updated

--- repominer/metrics/terraform.py
+++ repominer/metrics/terraform.py
@@ -1,19 +1,27 @@
 from .base import BaseMetricsExtractor
 from repominer.filters import is_terraform_file
+from terraform_metrics import general_metrics, configuration_metrics  # il file che mi hai appena dato
+
+# Metti qui solo le metriche che vuoi davvero calcolare
+METRICS_TO_COMPUTE = tuple(configuration_metrics.keys()) + tuple(general_metrics.keys())
 
 
 class TerraformMetricsExtractor(BaseMetricsExtractor):
 
     def get_product_metrics(self, script: str) -> dict:
+        results = {}
 
-        lines = script.splitlines()
+        # Unire tutti i dizionari delle metriche
+        all_metrics = {**general_metrics, **configuration_metrics}
 
-        return {
-            "lines_code": len(lines),
-            "num_resources": script.count("resource"),
-            "num_variables": script.count("variable"),
-            "num_providers": script.count("provider")
-        }
+        for metric_name in METRICS_TO_COMPUTE:
+            metric_class = all_metrics[metric_name]
+            try:
+                results[metric_name] = metric_class(script).count()
+            except Exception:
+                results[metric_name] = 0 # fallback se la metrica fallisce
+
+        return results
 
     def ignore_file(self, path_to_file: str, content: str = None):
         return not is_terraform_file(path_to_file)
--- repominer/mining/terraform.py
+++ repominer/mining/terraform.py
@@ -1,11 +1,86 @@
-from repominer.mining.base import BaseMiner
-from repominer import filters
+import hcl2
+from typing import List
+from pydriller.repository import Repository
+from pydriller.domain.commit import ModificationType
 
+from repominer import filters, utils
+from repominer.mining.base import BaseMiner, FixingCommitClassifier
+
+# Definiamo le "chiavi principali" dei file Terraform da monitorare
+CONFIG_TERRAFORM_KEYS = ['resource', 'module', 'variable', 'output', 'provider', 'data', 'locals']
 
 class TerraformMiner(BaseMiner):
-    """
-    Miner for Terraform repositories
-    """
+    """Estende BaseMiner per mining di repository Terraform"""
+
+    def __init__(self, url_to_repo: str, clone_repo_to: str, branch: str = None):
+        super().__init__(url_to_repo, clone_repo_to, branch)
+        self.FixingCommitClassifier = TerraformFixingCommitClassifier
 
     def ignore_file(self, path_to_file: str, content: str = None):
-        return not filters.is_terraform_file(path_to_file)
+        """Ignora file non Terraform (.tf)"""
+        return not path_to_file.endswith('.tf')
+
+
+class TerraformFixingCommitClassifier(FixingCommitClassifier):
+    """Classifica commit che modificano file Terraform rilevanti"""
+
+    def is_data_changed(self) -> bool:
+        """Controlla se sono cambiati blocchi significativi (resource/module/variable/output...)"""
+        for modified_file in self.commit.modified_files:
+            if modified_file.change_type != ModificationType.MODIFY or not modified_file.new_path.endswith('.tf'):
+                continue
+
+            try:
+                code_before = hcl2.loads(modified_file.source_code_before) if modified_file.source_code_before else {}
+                code_after = hcl2.loads(modified_file.source_code) if modified_file.source_code else {}
+
+                data_before = [value for key, value in utils.key_value_list(code_before) if key in CONFIG_TERRAFORM_KEYS]
+                data_after = [value for key, value in utils.key_value_list(code_after) if key in CONFIG_TERRAFORM_KEYS]
+
+                return data_before != data_after
+
+            except Exception:
+                # HCL malformato o errore di parsing -> ignora
+                pass
+
+        return False
+
+    def is_module_changed(self) -> bool:
+        """Controlla se sono cambiati i blocchi module"""
+        for modified_file in self.commit.modified_files:
+            if modified_file.change_type != ModificationType.MODIFY or not modified_file.new_path.endswith('.tf'):
+                continue
+
+            try:
+                code_before = hcl2.loads(modified_file.source_code_before) if modified_file.source_code_before else {}
+                code_after = hcl2.loads(modified_file.source_code) if modified_file.source_code else {}
+
+                modules_before = [value for key, value in utils.key_value_list(code_before) if key == 'module']
+                modules_after = [value for key, value in utils.key_value_list(code_after) if key == 'module']
+
+                return modules_before != modules_after
+
+            except Exception:
+                pass
+
+        return False
+
+    def is_resource_changed(self) -> bool:
+        """Controlla se sono cambiati i blocchi resource"""
+        for modified_file in self.commit.modified_files:
+            if modified_file.change_type != ModificationType.MODIFY or not modified_file.new_path.endswith('.tf'):
+                continue
+
+            try:
+                code_before = hcl2.loads(modified_file.source_code_before) if modified_file.source_code_before else {}
+                code_after = hcl2.loads(modified_file.source_code) if modified_file.source_code else {}
+
+                resources_before = [value for key, value in utils.key_value_list(code_before) if key == 'resource']
+                resources_after = [value for key, value in utils.key_value_list(code_after) if key == 'resource']
+
+                return resources_before != resources_after
+
+            except Exception:
+                pass
+
+        return False
