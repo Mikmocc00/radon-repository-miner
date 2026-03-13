cef2e79de76c6c80ec7eb3a8e963d2573824e212
sighzeero <59253489+sighzeero@users.noreply.github.com>
2026-03-13 11:42:34 +0100

    New Terraform Miner

--- repominer/mining/terraform.py
+++ repominer/mining/terraform.py
@@ -1,40 +1,152 @@
 import hcl2
-from typing import List
-from pydriller.repository import Repository
+from typing import Set, Tuple
 from pydriller.domain.commit import ModificationType
 
-from repominer import filters, utils
 from repominer.mining.base import BaseMiner, FixingCommitClassifier
 
-# Definiamo le "chiavi principali" dei file Terraform da monitorare
-CONFIG_TERRAFORM_KEYS = [
-    'resource',
-    'module',
-    'variable',
-    'output',
-    'provider',
-    'data',
-    'locals',
-    'terraform'
-]
+
+# attributi poco rilevanti per bug fixing
+IGNORED_ATTRIBUTES = {
+    "tags",
+    "description",
+    "name",
+    "comment"
+}
+
+
+DATA_KEYS = ["variable", "locals", "output"]
+
 
 class TerraformMiner(BaseMiner):
-    """Estende BaseMiner per mining di repository Terraform"""
 
     def __init__(self, url_to_repo: str, clone_repo_to: str, branch: str = None):
         super().__init__(url_to_repo, clone_repo_to, branch)
         self.FixingCommitClassifier = TerraformFixingCommitClassifier
 
     def ignore_file(self, path_to_file: str, content: str = None):
+
         if not path_to_file:
             return True
+
         return not path_to_file.endswith((".tf", ".tfvars"))
 
 
 class TerraformFixingCommitClassifier(FixingCommitClassifier):
-    """Classifica commit che modificano file Terraform rilevanti"""
+
+    # -----------------------------
+    # parsing
+    # -----------------------------
+
+    def _parse_hcl(self, source):
+
+        if not source:
+            return {}
+
+        try:
+            return hcl2.loads(source)
+        except Exception:
+            return {}
+
+    # -----------------------------
+    # extractors
+    # -----------------------------
+
+    def _extract_resources(self, parsed) -> Set[Tuple[str, str]]:
+
+        resources = set()
+
+        for block in parsed.get("resource", []):
+            for rtype in block:
+                for name in block[rtype]:
+                    resources.add((rtype, name))
+
+        return resources
+
+    def _extract_resource_attributes(self, parsed):
+
+        attributes = set()
+
+        for block in parsed.get("resource", []):
+
+            for rtype in block:
+                for name in block[rtype]:
+
+                    body = block[rtype][name]
+
+                    if isinstance(body, dict):
+
+                        for attr in body:
+
+                            if attr not in IGNORED_ATTRIBUTES:
+                                attributes.add((rtype, name, attr))
+
+        return attributes
+
+    def _extract_modules(self, parsed):
+
+        modules = set()
+
+        for block in parsed.get("module", []):
+            for name in block:
+                modules.add(name)
+
+        return modules
+
+    def _extract_providers(self, parsed):
+
+        providers = set()
+
+        for block in parsed.get("provider", []):
+            for name in block:
+                providers.add(name)
+
+        return providers
+
+    def _extract_data(self, parsed):
+
+        data = set()
+
+        for key in DATA_KEYS:
+
+            for block in parsed.get(key, []):
+                for name in block:
+                    data.add((key, name))
+
+        return data
+
+    # -----------------------------
+    # classification methods
+    # -----------------------------
 
     def is_data_changed(self) -> bool:
+
+        for modified_file in self.commit.modified_files:
+
+            if modified_file.change_type not in (
+                    ModificationType.MODIFY,
+                    ModificationType.ADD,
+                    ModificationType.DELETE
+            ):
+                continue
+
+            path = modified_file.new_path or modified_file.old_path
+
+            if not path or not path.endswith((".tf", ".tfvars")):
+                continue
+
+            code_before = self._parse_hcl(modified_file.source_code_before)
+            code_after = self._parse_hcl(modified_file.source_code)
+
+            data_before = self._extract_data(code_before)
+            data_after = self._extract_data(code_after)
+
+            if data_before != data_after:
+                return True
+
+        return False
+
+    def is_module_changed(self) -> bool:
+
         for modified_file in self.commit.modified_files:
 
             if modified_file.change_type not in (
@@ -49,50 +161,20 @@ class TerraformFixingCommitClassifier(FixingCommitClassifier):
             if not path or not path.endswith(".tf"):
                 continue
 
-            try:
-                code_before = hcl2.loads(modified_file.source_code_before) if modified_file.source_code_before else {}
-                code_after = hcl2.loads(modified_file.source_code) if modified_file.source_code else {}
+            code_before = self._parse_hcl(modified_file.source_code_before)
+            code_after = self._parse_hcl(modified_file.source_code)
 
-                data_before = [v for k,v in utils.key_value_list(code_before) if k in CONFIG_TERRAFORM_KEYS]
-                data_after = [v for k,v in utils.key_value_list(code_after) if k in CONFIG_TERRAFORM_KEYS]
+            modules_before = self._extract_modules(code_before)
+            modules_after = self._extract_modules(code_after)
 
-                if data_before != data_after:
-                    return True
+            if modules_before != modules_after:
+                return True
 
-            except Exception:
-                pass
+            providers_before = self._extract_providers(code_before)
+            providers_after = self._extract_providers(code_after)
 
-        return False
-
-    def is_module_changed(self) -> bool:
-
-        for modified_file in self.commit.modified_files:
-
-            if modified_file.change_type not in (
-                    ModificationType.MODIFY,
-                    ModificationType.ADD,
-                ModificationType.DELETE
-            ):
-                continue
-
-            path = modified_file.new_path or modified_file.old_path
-
-            if not path or not path.endswith(".tf"):
-                continue
-
-            try:
-
-                code_before = hcl2.loads(modified_file.source_code_before) if modified_file.source_code_before else {}
-                code_after = hcl2.loads(modified_file.source_code) if modified_file.source_code else {}
-
-                modules_before = [v for k,v in utils.key_value_list(code_before) if k == "module"]
-                modules_after = [v for k,v in utils.key_value_list(code_after) if k == "module"]
-
-                if modules_before != modules_after:
-                    return True
-
-            except Exception:
-                pass
+            if providers_before != providers_after:
+                return True
 
         return False
 
@@ -112,18 +194,19 @@ class TerraformFixingCommitClassifier(FixingCommitClassifier):
             if not path or not path.endswith(".tf"):
                 continue
 
-            try:
+            code_before = self._parse_hcl(modified_file.source_code_before)
+            code_after = self._parse_hcl(modified_file.source_code)
 
-                code_before = hcl2.loads(modified_file.source_code_before) if modified_file.source_code_before else {}
-                code_after = hcl2.loads(modified_file.source_code) if modified_file.source_code else {}
+            resources_before = self._extract_resources(code_before)
+            resources_after = self._extract_resources(code_after)
 
-                resources_before = [v for k,v in utils.key_value_list(code_before) if k == "resource"]
-                resources_after = [v for k,v in utils.key_value_list(code_after) if k == "resource"]
+            if resources_before != resources_after:
+                return True
 
-                if resources_before != resources_after:
-                    return True
+            attrs_before = self._extract_resource_attributes(code_before)
+            attrs_after = self._extract_resource_attributes(code_after)
 
-            except Exception:
-                pass
+            if attrs_before != attrs_after:
+                return True
 
         return False
