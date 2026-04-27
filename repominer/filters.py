be2928cefcf9d81c01d498571d4edf0056d51938
Mikmocc00 <michele.moccia03@gmail.com>
2026-04-27 11:48:10 +0200

    Update filters.py
    Removed docstrings from several file type check functions.

--- repominer/filters.py
+++ repominer/filters.py
@@ -2,23 +2,13 @@ import re
 
 
 def is_ansible_file(path: str) -> bool:
-    """
-    Check whether the path is an Ansible file
-    :param path: a path
-    :return: True if the path links to an Ansible file. False, otherwise
-    """
     return path \
         and ('test/' not in path) \
         and any(w in path for w in ['playbooks/', 'meta/', 'tasks/', 'handlers/', 'roles/']) and path.endswith('.yml')
 
 
 def is_tosca_file(path: str, content: str = None) -> bool:
-    """
-    Check whether the path is a TOSCA file
-    :param path: a path
-    :param content: eventually the source code
-    :return: True if the path links to a TOSCA file. False, otherwise
-    """
+
     if content:
         return re.match(r'^tosca_definitions_version\s*:.+', content) is not None
 
@@ -26,17 +16,12 @@ def is_tosca_file(path: str, content: str = None) -> bool:
 
 
 def is_terraform_file(path: str, content: str = None) -> bool:
-    """
-    Check whether the path is a Terraform file
-    """
+   
     return path and path.endswith(".tf")
 
 
 def is_kubernetes_file(path: str, content: str = None) -> bool:
-    """
-    Check whether the path is a Kubernetes file
-    """
-    # Controlla l'estensione e ignora le cartelle di test
+    
     if not path or 'test/' in path or not (path.endswith('.yaml') or path.endswith('.yml')):
         return False
 
@@ -49,15 +34,11 @@ def is_kubernetes_file(path: str, content: str = None) -> bool:
 
 
 def is_docker_file(path: str, content: str = None) -> bool:
-    """
-    Check whether the path is a Docker file
-    """
-    # Ignoriamo i percorsi vuoti o le cartelle di test
+  
     if not path or 'test/' in path.lower():
         return False
 
-    # Estraiamo solo il nome del file dal percorso completo
+  
     filename = path.split('/')[-1].lower()
 
-    # Riconosce sia il classico "Dockerfile" sia estensioni come "app.dockerfile"
     return filename == 'dockerfile' or filename.endswith('.dockerfile')
